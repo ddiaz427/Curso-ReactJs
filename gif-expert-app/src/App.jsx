86df2b38c1b7fad10225a368d5ebd00def11d654
Dario Alexander Diaz Florez <ingdariodiaz@gmail.com>
2023-04-25 16:24:38 -0500

    Gifs search with React

---
+++ gif-expert-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { GifExpertApp } from './GifExpertApp';
+import './styles.css'
+
+function App() {
+  return (
+    <div className="App">
+      <GifExpertApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ gif-expert-app/src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async( category ) => {
+    const apiKey = 'ZCZ3611GVQNcaShFM9LoYYraSQzFmCSY'
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ category }&limit=10`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    return gifs;
+}
